ab37e46f80941d3563a2f43a04549491005079d0
C1316299 Jayme Elias de Oliveira Neto <c1316299@interno.bb.com.br>
2020-08-15 22:54:50 -0300

    Adicionando scrat do Wikipedia na pagina individual de cada console

--- game-scrap.py
+++ game-scrap.py
@@ -2,16 +2,49 @@ import bs4
 import re
 import urllib.request as urllib_request
 import pandas as pd
-
 from urllib.request import urlopen, Request
 from urllib.error import URLError, HTTPError
 from bs4 import BeautifulSoup
 
-url2 = 'https://en.wikipedia.org/wiki/Video_game_console'
+def extrai_infos_console(nome_console):
+    url2 = 'https://en.wikipedia.org/wiki/' + nome_console
+
+    try:
+        req = Request(url2)
+        resp = urlopen(req)
+    except URLError as e:
+        print(e.reason)
+    except HTTPError as e:
+        print(e.status, e.reason)
+
+    html = resp.read()
+    html = html.decode('utf-8')
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    lista_infos = ['Console', 'Developer', 'Generation', 'Release date', 'Introductory price']
+
+    dic_consoles = {}
+    info_box = soup.find('table', class_='infobox hproduct vevent')
+    dic_consoles['Console'] = [info_box.find('caption').getText()]
+    infos = info_box.find('tbody').findAll('tr')
+
+    for inf in infos:
+        try:
+            th = inf.find('th').getText()
+        except:
+            continue
+        if th in lista_infos:
+            dic_consoles[th] = [inf.find('td').getText()]
+    return pd.DataFrame(dic_consoles)
+
+
+
+url = 'https://en.wikipedia.org/wiki/Video_game_console'
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36'}
 
 try:
-    req = Request(url2)
+    req = Request(url)
     resp = urlopen(req)
 except URLError as e:
     print(e.reason)
@@ -32,15 +65,11 @@ for linha in tabela_consoles:
     if len(linha) == 5:
         consoles.append(linha)
 
-dic_consoles = {'Console':[], 'Release year': [], 'Original price': [], 'Inflation 2020': [], 'Global Sales': []}
-for console in consoles:
-   dic_consoles['Console'].append(console[0].getText('td'))
-   dic_consoles['Release year'].append(int(console[1].getText('td')))
-   dic_consoles['Original price'].append(console[2].getText('td'))
-   dic_consoles['Inflation 2020'].append(console[3].getText('td'))
-   quant = re.sub(',','',console[4].getText('td'))
-   quant = re.match('(\d+)', quant).group()
-   dic_consoles['Global Sales'].append(int(quant))
-df_consoles = pd.DataFrame(dic_consoles)
 
-df_consoles.to_csv('consoles.csv', index=False)
+df = pd.DataFrame()
+for console in consoles:
+    nome = re.sub(' ', '_',console[0].getText('td'))
+    try:
+        df = df.append(extrai_infos_console(nome), ignore_index=True)
+    except Exception as e:
+        print('Erro na extração de: ' + nome + ' --- ')
